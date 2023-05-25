1db206be9a9021bf236034acfd7ea6c229ee96f6
Jackson Tian <shyvo1987@gmail.com>
2023-05-25 12:42:43 +0800

    增加沃通根证书跑通客户端认证

--- src/test/java/com/aliyun/gmsse/MainTest.java
+++ src/test/java/com/aliyun/gmsse/MainTest.java
@@ -1,26 +1,45 @@
 package com.aliyun.gmsse;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MainTest {
     @Test
-    public void test() throws IOException, NoSuchAlgorithmException, KeyManagementException {
+    public void test() throws IOException, NoSuchAlgorithmException, KeyManagementException, KeyStoreException, CertificateException {
         GMProvider provider = new GMProvider();
         SSLContext sc = SSLContext.getInstance("TLS", provider);
-        sc.init(null, null, null);
+
+        BouncyCastleProvider bc = new BouncyCastleProvider();
+        KeyStore ks = KeyStore.getInstance("JKS");
+        CertificateFactory cf = CertificateFactory.getInstance("X.509", bc);
+        FileInputStream is = new FileInputStream(this.getClass().getClassLoader().getResource("WoTrus-SM2.crt").getFile());
+        X509Certificate cert = (X509Certificate) cf.generateCertificate(is);
+        ks.load(null, null);
+        ks.setCertificateEntry("gmca", cert);
+
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("X509", provider);
+        tmf.init(ks);
+        sc.init(null, tmf.getTrustManagers(), null);
         SSLSocketFactory ssf = sc.getSocketFactory();
 
-        URL serverUrl = new URL("https://sm2test.ovssl.cn/");
+        URL serverUrl = new URL("https://sm2only.ovssl.cn/");
         HttpsURLConnection conn = (HttpsURLConnection) serverUrl.openConnection();
         conn.setRequestMethod("GET");
         // set SSLSocketFactory
