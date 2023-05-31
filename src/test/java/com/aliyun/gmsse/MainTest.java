1a1f3dfa66b55ebc19be5a3cd8433faeb36278f0
Jackson Tian <shyvo1987@gmail.com>
2023-05-31 17:39:25 +0800

    refine code for handshake

--- src/test/java/com/aliyun/gmsse/MainTest.java
+++ src/test/java/com/aliyun/gmsse/MainTest.java
@@ -1,7 +1,6 @@
 package com.aliyun.gmsse;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
