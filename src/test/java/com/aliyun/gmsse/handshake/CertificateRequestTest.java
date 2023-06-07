677124a9d8445d0a49a1f75d14e3c2c47a901faa
Jackson Tian <shyvo1987@gmail.com>
2023-06-07 22:58:43 +0800

    fix test cases for CertificateRequest

--- src/test/java/com/aliyun/gmsse/handshake/CertificateRequestTest.java
+++ src/test/java/com/aliyun/gmsse/handshake/CertificateRequestTest.java
@@ -8,9 +8,10 @@ import org.junit.Test;
 public class CertificateRequestTest {
 
     @Test
-    public void nullTest() throws Exception{
+    public void getBytesTest() throws Exception{
         short[] types = new short[] {1, 2, 3};
         Vector<byte[]> v = new Vector<>();
-        Assert.assertNull(new CertificateRequest(types, v).getBytes());
+        byte[] expected = new byte[] {0x03, 0x01, 0x02, 0x03, 0x00, 0x00};
+        Assert.assertArrayEquals(expected, new CertificateRequest(types, v).getBytes());
     }
 }
