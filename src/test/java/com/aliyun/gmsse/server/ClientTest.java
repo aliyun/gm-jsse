78f28f7fb5671ab07d10b350c93ecfc9cb009392
Jackson Tian <shyvo1987@gmail.com>
2023-06-07 10:08:59 +0800

    Improve server test cases via thread

--- src/test/java/com/aliyun/gmsse/server/ClientTest.java
+++ src/test/java/com/aliyun/gmsse/server/ClientTest.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -12,27 +13,74 @@ import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.security.spec.InvalidKeySpecException;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManagerFactory;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.Assert;
+import org.junit.Test;
 
 import com.aliyun.gmsse.GMProvider;
 
 public class ClientTest {
 
-    public static void main(String[] args) throws NoSuchAlgorithmException, KeyStoreException, CertificateException,
+    public static Runnable runServer() {
+        return new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Server server = new Server();
+                    SSLServerSocket ss = server.buildServerSocket();
+
+                    SSLSocket socket = (SSLSocket) ss.accept();
+
+                    // get path to class file from header
+                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    String line;
+                    do {
+                        line = in.readLine();
+                        System.out.println(line);
+                    } while ((line.length() != 0) &&
+                            (line.charAt(0) != '\r') && (line.charAt(0) != '\n'));
+
+                    PrintWriter ps = new PrintWriter(socket.getOutputStream(), false);
+                    String content = "<!DOCTYPE html>\r\n" + "Hi.\r\n";
+                    int contentLength = content.getBytes().length;
+                    ps.print("HTTP/1.1 200 OK\r\n");
+                    ps.print("Content-Type: text/html\r\n");
+                    ps.print("Connection: close\r\n");
+                    ps.print("Content-Length:" + contentLength + "\r\n");
+                    ps.print("\r\n");
+                    ps.print(content);
+                    ps.flush();
+                    socket.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+    }
+
+    @Test
+    public void testServer() throws NoSuchAlgorithmException, KeyStoreException, CertificateException,
             IOException, KeyManagementException, InterruptedException, URISyntaxException {
         GMProvider provider = new GMProvider();
         SSLContext sc = SSLContext.getInstance("TLS", provider);
 
+        Runnable runner = runServer();
+        Thread thread = new Thread(runner, "server");
+        thread.start();
+        Thread.sleep(1000);
         BouncyCastleProvider bc = new BouncyCastleProvider();
         KeyStore ks = KeyStore.getInstance("JKS");
         CertificateFactory cf = CertificateFactory.getInstance("X.509", bc);
@@ -47,7 +95,6 @@ public class ClientTest {
         SSLSocketFactory ssf = sc.getSocketFactory();
 
         URI uri = new URI("https://localhost:8443/");
-        // URL serverUrl = new URL("https://sm2only.ovssl.cn/");
         URL serverUrl = uri.toURL();
         HttpsURLConnection conn = (HttpsURLConnection) serverUrl.openConnection();
         conn.setSSLSocketFactory(ssf);
@@ -66,5 +113,7 @@ public class ClientTest {
         }
         Assert.assertEquals("<!DOCTYPE html>Hi.", buffer.toString());
         connInputStream.close();
+        // 中断服务线程
+        thread.interrupt();
     }
 }
--- src/test/java/com/aliyun/gmsse/server/Server.java
+++ src/test/java/com/aliyun/gmsse/server/Server.java
@@ -1,14 +1,10 @@
 package com.aliyun.gmsse.server;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.security.KeyFactory;
 import java.security.KeyManagementException;
@@ -28,24 +24,23 @@ import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
-import javax.net.ssl.SSLSocket;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import com.aliyun.gmsse.GMProvider;
 
-public class ServerTest {
+public class Server {
 
-    public static X509Certificate loadCertificate(String path) throws KeyStoreException, CertificateException, FileNotFoundException {
+    public X509Certificate loadCertificate(String path) throws KeyStoreException, CertificateException, FileNotFoundException {
         BouncyCastleProvider bc = new BouncyCastleProvider();
         CertificateFactory cf = CertificateFactory.getInstance("X.509", bc);
-        InputStream is = ServerTest.class.getClassLoader().getResourceAsStream(path);
+        InputStream is = Server.class.getClassLoader().getResourceAsStream(path);
         X509Certificate cert = (X509Certificate) cf.generateCertificate(is);
         return cert;
     }
 
-    public static PrivateKey loadPrivateKey(String path) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
-        InputStream is = ServerTest.class.getClassLoader().getResourceAsStream(path);
+    public PrivateKey loadPrivateKey(String path) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        InputStream is = Server.class.getClassLoader().getResourceAsStream(path);
         InputStreamReader inputStreamReader = new InputStreamReader(is);
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
         StringBuilder sb = new StringBuilder();
@@ -65,21 +60,7 @@ public class ServerTest {
         return privateKey;
     }
 
-        /**
-     * Returns the path to the file obtained from
-     * parsing the HTML header.
-     */
-    private static void getPath(BufferedReader in)throws IOException
-    {
-        String line;
-        do {
-            line = in.readLine();
-            System.out.println(line);
-        } while ((line.length() != 0) &&
-                 (line.charAt(0) != '\r') && (line.charAt(0) != '\n'));
-    }
-
-    public static void main(String[] args) throws NoSuchAlgorithmException, KeyStoreException, CertificateException, IOException, UnrecoverableKeyException, KeyManagementException, InvalidKeySpecException {
+    public SSLServerSocket buildServerSocket() throws NoSuchAlgorithmException, KeyStoreException, CertificateException, IOException, UnrecoverableKeyException, KeyManagementException, InvalidKeySpecException {
         GMProvider provider = new GMProvider();
         SSLContext sc = SSLContext.getInstance("TLS", provider);
 
@@ -101,25 +82,7 @@ public class ServerTest {
         SSLServerSocket ss = (SSLServerSocket) ssf.createServerSocket(8443);
         // ss.setNeedClientAuth(true);
         // ss.setEnabledProtocols(new String[] { "TLSv1.2" });
-        while (true) {
-            SSLSocket socket = (SSLSocket) ss.accept();
-
-            // get path to class file from header
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            getPath(in);
-
-            PrintWriter ps = new PrintWriter(socket.getOutputStream(),false);
-            String content = "<!DOCTYPE html>\r\n" + "Hi.\r\n";
-            int contentLength = content.getBytes().length;
-            ps.print("HTTP/1.1 200 OK\r\n");
-            ps.print("Content-Type: text/html\r\n");
-            ps.print("Connection: close\r\n");
-            ps.print("Content-Length:" + contentLength + "\r\n");
-            ps.print("\r\n");
-            ps.print(content);
-            ps.flush();
-            socket.close();
-        }
+        return ss;
     }
 
 }
